83aaf6bbd61b667034a109015b840e254bac73aa
cmo <cen.mo@cern.ch>
2026-05-07 15:15:21 -0700

    closure plot for responsematrices

--- processor/ResponseMatricesManager.py
+++ processor/ResponseMatricesManager.py
@@ -112,3 +112,66 @@ class ResponseMatricesManager:
         var_values = ak.to_numpy(events[var], allow_missing=False)
         binned_var = unfold.bin_variable(var_values, self.bin_edges)
         return binned_var.astype(float)
+
+
+    def closure_test(self, region, signal_name, var):
+        signal_category = get_event_category_from_signal_name(signal_name)
+        response_matrix = self.get_response_matrix(region, signal_name, var)
+        if self.raw_ztautau_events is None:
+            self.raw_ztautau_events, _ = DataLoader.DataLoader.load_processed_data(self.data_dir, "Ztautau", "raw")
+        raw_events = self.raw_ztautau_events
+        mask_target_signal = raw_events['event_category'] == signal_category
+        raw_events = raw_events[mask_target_signal]
+        
+
+        mask_truth_region = raw_events['truth_QI_region'] == 1
+        mask_analysis_region = (raw_events[f'{region}_cut'] == 1) & (raw_events['flags_valid'] > 0) & (raw_events['theta_cm'] > 0.6) & (raw_events['mtautau'] > 80)
+        # events = raw_events[mask_target_signal & (mask_truth_region | mask_analysis_region)]
+        events = raw_events
+        weight = ak.to_numpy(events['weight_nominal'], allow_missing=False)
+
+        # build truth distribution using truth region events
+        var_truth_binned = self.get_binned_observable(f'truth_{var}', events[mask_truth_region])
+        h_truth = unfold.build_TH1D(f"h_truth_{region}_{signal_name}_{var}", var_truth_binned, num_bins=self.num_bins, weight=weight[mask_truth_region])
+
+        # build reco distribution using analysis region events and response matrix
+        var_recon_binned = self.get_binned_observable(var, events[mask_analysis_region])
+        h_recon = unfold.build_TH1D(f"h_recon_{region}_{signal_name}_{var}", var_recon_binned, num_bins=self.num_bins, weight=weight[mask_analysis_region])
+
+        # unfold reco distribution and compare with truth distribution
+        unfold_result = ROOT.RooUnfoldSvd(response_matrix, h_recon, 5).Hunfold(2)
+        # roounfold = ROOT.RooUnfoldInvert(response_matrix, h_recon)
+        # unfold_result = roounfold.Hunfold()
+
+        # forward fold the truth distribution and compare with reco distribution
+        h_truth_forward_folded = response_matrix.ApplyToTruth(h_truth)
+
+        # plot
+        ROOT.gStyle.SetOptStat(0)
+        c = ROOT.TCanvas(f"closure_{region}_{signal_name}_{var}", f"closure_{region}_{signal_name}_{var}", 800, 600)
+        h_truth.GetYaxis().SetRangeUser(0, max(h_truth.GetMaximum(), h_recon.GetMaximum(), unfold_result.GetMaximum())*1.2)
+
+        h_truth.SetLineColor(ROOT.kBlack)
+        h_truth.Draw("hist E1")
+        h_recon.SetLineColor(ROOT.kRed)
+        h_recon.Draw("hist same E1")
+        unfold_result.SetLineColor(ROOT.kBlue)
+        unfold_result.Draw("hist same E1")
+        h_truth_forward_folded.SetLineColor(ROOT.kGreen)
+        h_truth_forward_folded.Draw("hist same  E1")
+        c.BuildLegend()
+        c.SaveAs(f"{self.output_dir}/closure_{region}_{signal_name}_{var}.png")
+        
+
+if __name__ == "__main__":
+    data_dir = "./20260427-dataset/"
+    data_dir = "tmp"
+    output_dir = "./20260427-forwardfold/"
+    dict_region_to_signals = {
+        "pipi": ["Ztautau_pipi"],
+    }
+    response_manager = ResponseMatricesManager(data_dir, output_dir, dict_region_to_signals)
+    for obs in response_manager.unfold_vars:
+        response_manager.closure_test("pipi", "Ztautau_pipi", obs)
+
+
