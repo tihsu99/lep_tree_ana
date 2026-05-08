d588c57deb39e50e764776960fcf6cd3be56c396
tihsu99 <b07202037@ntu.edu.tw>
2026-05-08 22:43:17 +0200

    fix: refactor tau momentum calculations to simplify and clarify logic

--- ml_pipeline/util/check_target_missing_vs_truth_observables.py
+++ ml_pipeline/util/check_target_missing_vs_truth_observables.py
@@ -190,30 +190,39 @@ def build_tau_tau_pair(tau_a: ak.Array, tau_b: ak.Array) -> ak.Array:
     py_b = pt_b * np.sin(tau_b.phi)
     pz_b = pt_b * np.sinh(eta_b)
 
-    px_shift = (px_a + px_b)
-    py_shift = (py_a + py_b)
-    pz_shift = (pz_a + pz_b)
+    px_a_final = px_a
+    py_a_final = py_a
+    pz_a_final = pz_a
 
-    px_a_corr = px_a - (px_shift/2)
-    py_a_corr = py_a - (py_shift / 2)
-    pz_a_corr = pz_a - (pz_shift/2)
+    px_b_final = px_b
+    py_b_final = py_b
+    pz_b_final = pz_b
 
-    # use corrected direction of tau_a only
-    norm_a_corr = np.sqrt(px_a_corr ** 2 + py_a_corr ** 2 + pz_a_corr ** 2)
 
-    ux_a = px_a_corr / norm_a_corr
-    uy_a = py_a_corr / norm_a_corr
-    uz_a = pz_a_corr / norm_a_corr
-
-    # rebuild with fixed |p|
-    px_a_final = p * ux_a
-    py_a_final = p * uy_a
-    pz_a_final = p * uz_a
-
-    # force tau_b to be exactly opposite
-    px_b_final = -px_a_final
-    py_b_final = -py_a_final
-    pz_b_final = -pz_a_final
+    # px_shift = (px_a + px_b)
+    # py_shift = (py_a + py_b)
+    # pz_shift = (pz_a + pz_b)
+    #
+    # px_a_corr = px_a - (px_shift/2)
+    # py_a_corr = py_a - (py_shift / 2)
+    # pz_a_corr = pz_a - (pz_shift/2)
+    #
+    # # use corrected direction of tau_a only
+    # norm_a_corr = np.sqrt(px_a_corr ** 2 + py_a_corr ** 2 + pz_a_corr ** 2)
+    #
+    # ux_a = px_a_corr / norm_a_corr
+    # uy_a = py_a_corr / norm_a_corr
+    # uz_a = pz_a_corr / norm_a_corr
+    #
+    # # rebuild with fixed |p|
+    # px_a_final = p * ux_a
+    # py_a_final = p * uy_a
+    # pz_a_final = p * uz_a
+    #
+    # # force tau_b to be exactly opposite
+    # px_b_final = -px_a_final
+    # py_b_final = -py_a_final
+    # pz_b_final = -pz_a_final
 
     tau_a_final = ak.zip(
         {
